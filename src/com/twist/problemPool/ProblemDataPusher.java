6a03d1d800795b3544db2ebbfa7f72f368bb891e
Evgeny Savelev <evgenytech@gmail.com>
2013-09-01 16:02:29 -0400

    A few changes

--- src/com/twist/problemPool/ProblemDataPusher.java
+++ src/com/twist/problemPool/ProblemDataPusher.java
@@ -4,6 +4,6 @@ import java.util.HashMap;
 
 public class ProblemDataPusher
 {
-	static HashMap<Integer,String> problemDataStorage = new HashMap<Integer,String>();
+	static HashMap<Integer,ProblemData> problemDataStorage = new HashMap<Integer,ProblemData>();
 	static HashMap<Integer,String> problemTestsStorage = new HashMap<Integer,String>();
 }
