6aa2c5baa64af7f38e819157ab261d156e93551c
Evgeny Savelev <evgenytech@gmail.com>
2013-09-05 10:53:27 -0400

    working on problems with dependencies working on mathematica generating code things are a bit undone
    added comments to problemData class

--- src/com/twist/problemPool/CheckAnswers.java
+++ src/com/twist/problemPool/CheckAnswers.java
@@ -15,67 +15,83 @@ import com.twist.kernelUtilities.KernelTalker;
  * Servlet implementation class CheckAnswers
  */
 @WebServlet("/CheckAnswers")
-public class CheckAnswers extends HttpServlet {
+public class CheckAnswers extends HttpServlet
+{
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public CheckAnswers() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public CheckAnswers()
+	{
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException
+	{
 		// TODO Auto-generated method stub
 		response.setContentType("text/plain");
-		
-		int id=Integer.parseInt(request.getParameter("id"));
-		String args="",evalString=ProblemDataPusher.problemDataStorage.get(id);
-		String test=ProblemDataPusher.problemTestsStorage.get(id);
-		int i,numberOfFields=Integer.parseInt(request.getParameter("total"));
 
-		for(i=1;i<=numberOfFields;i++)
+		int id = Integer.parseInt(request.getParameter("id"));
+		String args = "", evalString = ProblemDataPusher.problemDataStorage.get(id);
+		String test = ProblemDataPusher.problemTestsStorage.get(id);
+		int i, numberOfFields = Integer.parseInt(request.getParameter("total"));
+
+		for (i = 1; i <= numberOfFields; i++)
 		{
- 			args+=",\"" + request.getParameter("f"+i)+"\"";
- 		}
-		args = args.replaceAll("^[,]*","{");
-		args+="}";
+			args += ",\"" + request.getParameter("f" + i) + "\"";
+		}
+		args = args.replaceAll("^[,]*", "{");
+		args += "}";
 
 		PrintWriter out = response.getWriter();
-		KernelTalker kTalker=new KernelTalker();
-		out.print(test+args+"\n");
-		out.print(kTalker.executeCommand(test+args));
+		KernelTalker kTalker = new KernelTalker();
+		out.print(test + args + "\n");
+		out.print(kTalker.executeCommand(test + args));
 		out.close();
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException
+	{
 		// TODO Auto-generated method stub
 		response.setContentType("text/plain");
+
+		int id = Integer.parseInt(request.getParameter("id"));
+		ProblemData pData;
+		ProblemDataPusher pPusher = new ProblemDataPusher();
 		
-		int id=Integer.parseInt(request.getParameter("id"));
-		String args="",evalString=ProblemDataPusher.problemDataStorage.get(id);
-		String test=ProblemDataPusher.problemTestsStorage.get(id);
-		int i,numberOfFields=Integer.parseInt(request.getParameter("total"));
+		String args = "", evalString = ProblemDataPusher.problemDataStorage.get(id);
+		String test = ProblemDataPusher.problemTestsStorage.get(id);
+		int i, numberOfFields = Integer.parseInt(request.getParameter("total"));
 
 		args = String.valueOf(numberOfFields);
-		for(i=1;i<=numberOfFields;i++)
+		for (i = 1; i <= numberOfFields; i++)
 		{
- 			args+=",\"" + request.getParameter("f"+i)+"\"";
- 		}
+			args += ",\"" + request.getParameter("f" + i) + "\"";
+		}
 		args = "{" + args;
-		args+="}";
+		args += "}";
 
 		PrintWriter out = response.getWriter();
-		KernelTalker kTalker=new KernelTalker();
-//		out.print(test+args+"\n");
-		out.print("<p>" + kTalker.executeCommand(test+args) + "</p><p>" + test + "</p>");
+		KernelTalker kTalker = new KernelTalker();
+		pData = pPusher.get(id);
+		if(pData != null)
+		{
+			test = pData.getTestString();
+		}
+		// out.print(test+args+"\n");
+		out.print("<p>" + kTalker.executeCommand(test + args) + "</p><p>"+ test + "</p>");
 		out.close();
 	}
 
--- src/com/twist/problemPool/ProblemDataPusher.java
+++ src/com/twist/problemPool/ProblemDataPusher.java
@@ -4,6 +4,17 @@ import java.util.HashMap;
 
 public class ProblemDataPusher
 {
-	static HashMap<Integer,ProblemData> problemDataStorage = new HashMap<Integer,ProblemData>();
+	static HashMap<Integer,String> problemDataStorage = new HashMap<Integer,String>();
 	static HashMap<Integer,String> problemTestsStorage = new HashMap<Integer,String>();
+	static private HashMap<Integer,ProblemData> problemStorage = new HashMap<Integer,ProblemData>();
+	
+	public void push(int key, ProblemData pData)
+	{
+		problemStorage.put(key, pData);
+	}
+	
+	public ProblemData get(int key)
+	{
+		return problemStorage.get(key);
+	}
 }
